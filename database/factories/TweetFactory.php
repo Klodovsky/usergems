c13040b30c9577fc67d4e0232904c8091b295db4
Klodovskyy <55706752+Klodovskyy@users.noreply.github.com>
2022-02-19 11:36:54 +0100

    DB seed

---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tweet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TweetFactory extends Factory
+{
+
+    protected $model = Tweet::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'content' => $this->faker->text
+        ];
+    }
+}

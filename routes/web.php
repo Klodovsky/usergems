e1a7d163ba96d93b5ef102f0dc5dbcccb97c29d6
Klodovskyy <55706752+Klodovskyy@users.noreply.github.com>
2022-02-19 13:22:08 +0100

    routes middleware

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,16 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
 
-Route::get('tweets',[\App\Http\Controllers\TweetController::class,'index'])
-    ->name('tweets.index');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/tweets', [\App\Http\Controllers\TweetController::class, 'index'])->name('tweets.index');
+    Route::post('/tweets', [\App\Http\Controllers\TweetController::class, 'store'])->name('tweets.store');
+
+});
